507ee98d3d4aba0a99a941c9db3b142a4bb86d4d
DevKCS <80586242+DevKCS@users.noreply.github.com>
2023-10-19 23:27:22 +0900

    Create search9.js

---
+++ search9.js
@@ -0,0 +1,537 @@
+upperAreaList = {
+  "11": "서울특별시",
+  "21": "부산광역시",
+  "22": "대구광역시",
+  "23": "인천광역시",
+  "24": "광주광역시",
+  "25": "대전광역시",
+  "26": "울산광역시",
+  "29": "세종특별자치시",
+  "31": "경기도",
+  "32": "강원도",
+  "33": "충청북도",
+  "34": "충청남도",
+  "35": "전라북도",
+  "36": "전라남도",
+  "37": "경상북도",
+  "38": "경상남도",
+  "39": "제주도"
+};
+lowerAreaList = {
+  "11": {
+      "11010": "종로구",
+      "11020": "중구",
+      "11030": "용산구",
+      "11040": "성동구",
+      "11050": "광진구",
+      "11060": "동대문구",
+      "11070": "중랑구",
+      "11080": "성북구",
+      "11090": "강북구",
+      "11100": "도봉구",
+      "11110": "노원구",
+      "11120": "은평구",
+      "11130": "서대문구",
+      "11140": "마포구",
+      "11150": "양천구",
+      "11160": "강서구",
+      "11170": "구로구",
+      "11180": "금천구",
+      "11190": "영등포구",
+      "11200": "동작구",
+      "11210": "관악구",
+      "11220": "서초구",
+      "11230": "강남구",
+      "11240": "송파구",
+      "11250": "강동구"
+  },
+  "21": {
+      "21010": "중구",
+      "21020": "서구",
+      "21030": "동구",
+      "21040": "영도구",
+      "21050": "부산진구",
+      "21060": "동래구",
+      "21070": "남구",
+      "21080": "북구",
+      "21090": "해운대구",
+      "21100": "사하구",
+      "21110": "금정구",
+      "21120": "강서구",
+      "21130": "연제구",
+      "21140": "수영구",
+      "21150": "사상구",
+      "21310": "기장군"
+  },
+  "22": {
+      "22010": "중구",
+      "22020": "동구",
+      "22030": "서구",
+      "22040": "남구",
+      "22050": "북구",
+      "22060": "수성구",
+      "22070": "달서구",
+      "22310": "달성군"
+  },
+  "23": {
+      "23010": "중구",
+      "23020": "동구",
+      "23040": "연수구",
+      "23050": "남동구",
+      "23060": "부평구",
+      "23070": "계양구",
+      "23080": "서구",
+      "23090": "미추홀구",
+      "23310": "강화군",
+      "23320": "옹진군"
+  },
+  "24": {
+      "24010": "동구",
+      "24020": "서구",
+      "24030": "남구",
+      "24040": "북구",
+      "24050": "광산구"
+  },
+  "25": {
+      "25010": "동구",
+      "25020": "중구",
+      "25030": "서구",
+      "25040": "유성구",
+      "25050": "대덕구"
+  },
+  "26": {
+      "26010": "중구",
+      "26020": "남구",
+      "26030": "동구",
+      "26040": "북구",
+      "26310": "울주군"
+  },
+  "29": {
+      "29010": "세종시"
+  },
+  "31": {
+      "31010": "수원시",
+      "31020": "성남시",
+      "31030": "의정부시",
+      "31040": "안양시",
+      "31050": "부천시",
+      "31060": "광명시",
+      "31070": "평택시",
+      "31080": "동두천시",
+      "31090": "안산시",
+      "31100": "고양시",
+      "31110": "과천시",
+      "31120": "구리시",
+      "31130": "남양주시",
+      "31140": "오산시",
+      "31150": "시흥시",
+      "31160": "군포시",
+      "31170": "의왕시",
+      "31180": "하남시",
+      "31190": "용인시",
+      "31200": "파주시",
+      "31210": "이천시",
+      "31220": "안성시",
+      "31230": "김포시",
+      "31240": "화성시",
+      "31250": "광주시",
+      "31260": "양주시",
+      "31270": "포천시",
+      "31280": "여주시",
+      "31350": "연천군",
+      "31370": "가평군",
+      "31380": "양평군"
+  },
+  "32": {
+      "32010": "춘천시",
+      "32020": "원주시",
+      "32030": "강릉시",
+      "32040": "동해시",
+      "32050": "태백시",
+      "32060": "속초시",
+      "32070": "삼척시",
+      "32310": "홍천군",
+      "32320": "횡성군",
+      "32330": "영월군",
+      "32340": "평창군",
+      "32350": "정선군",
+      "32360": "철원군",
+      "32370": "화천군",
+      "32380": "양구군",
+      "32390": "인제군",
+      "32400": "고성군",
+      "32410": "양양군"
+  },
+  "33": {
+      "33020": "충주시",
+      "33030": "제천시",
+      "33040": "청주시",
+      "33320": "보은군",
+      "33330": "옥천군",
+      "33340": "영동군",
+      "33350": "진천군",
+      "33360": "괴산군",
+      "33370": "음성군",
+      "33380": "단양군",
+      "33390": "증평군"
+  },
+  "34": {
+      "34010": "천안시",
+      "34020": "공주시",
+      "34030": "보령시",
+      "34040": "아산시",
+      "34050": "서산시",
+      "34060": "논산시",
+      "34070": "계룡시",
+      "34080": "당진시",
+      "34310": "금산군",
+      "34330": "부여군",
+      "34340": "서천군",
+      "34350": "청양군",
+      "34360": "홍성군",
+      "34370": "예산군",
+      "34380": "태안군"
+  },
+  "35": {
+      "35010": "전주시",
+      "35020": "군산시",
+      "35030": "익산시",
+      "35040": "정읍시",
+      "35050": "남원시",
+      "35060": "김제시",
+      "35310": "완주군",
+      "35320": "진안군",
+      "35330": "무주군",
+      "35340": "장수군",
+      "35350": "임실군",
+      "35360": "순창군",
+      "35370": "고창군",
+      "35380": "부안군"
+  },
+  "36": {
+      "36010": "목포시",
+      "36020": "여수시",
+      "36030": "순천시",
+      "36040": "나주시",
+      "36060": "광양시",
+      "36310": "담양군",
+      "36320": "곡성군",
+      "36330": "구례군",
+      "36350": "고흥군",
+      "36360": "보성군",
+      "36370": "화순군",
+      "36380": "장흥군",
+      "36390": "강진군",
+      "36400": "해남군",
+      "36410": "영암군",
+      "36420": "무안군",
+      "36430": "함평군",
+      "36440": "영광군",
+      "36450": "장성군",
+      "36460": "완도군",
+      "36470": "진도군",
+      "36480": "신안군"
+  },
+  "37": {
+      "37010": "포항시",
+      "37020": "경주시",
+      "37030": "김천시",
+      "37040": "안동시",
+      "37050": "구미시",
+      "37060": "영주시",
+      "37070": "영천시",
+      "37080": "상주시",
+      "37090": "문경시",
+      "37100": "경산시",
+      "37310": "군위군",
+      "37320": "의성군",
+      "37330": "청송군",
+      "37340": "영양군",
+      "37350": "영덕군",
+      "37360": "청도군",
+      "37370": "고령군",
+      "37380": "성주군",
+      "37390": "칠곡군",
+      "37400": "예천군",
+      "37410": "봉화군",
+      "37420": "울진군",
+      "37430": "울릉군"
+  },
+  "38": {
+      "38030": "진주시",
+      "38050": "통영시",
+      "38060": "사천시",
+      "38070": "김해시",
+      "38080": "밀양시",
+      "38090": "거제시",
+      "38100": "양산시",
+      "38110": "창원시",
+      "38310": "의령군",
+      "38320": "함안군",
+      "38330": "창녕군",
+      "38340": "고성군",
+      "38350": "남해군",
+      "38360": "하동군",
+      "38370": "산청군",
+      "38380": "함양군",
+      "38390": "거창군",
+      "38400": "합천군"
+  },
+  "39": {
+      "39010": "제주시",
+      "39020": "서귀포시"
+  }
+};
+lowerArea = {
+  "29010": "세종시"
+};
+var PagingHelper = {
+  'data': {
+    currentPage: 1     // 현재페이지
+    //,startPage : 1    // 시작페이지
+    , pageSize: 5       // 페이지 사이즈 (화면 출력 페이지 수)
+    , maxListCount: 15  // (보여질)최대 리스트 수 (한페이지 출력될 항목 갯수)
+    , startnum: 1       // 시작 글번호
+    , lastnum: 15       // 마지막 글번호
+    , totalCnt: 0       // 전체 글의 갯수.
+    , totalPageCnt: 0   // 전체 페이지 수    
+  },
+  'setOption': function (opt) {
+    if (typeof opt != 'object') return;
+    for (key in opt) {
+      if (key in this.data) {
+        this.data[key] = opt[key]; //data에 입력받은 설정값 할당.
+      }
+    }
+  },
+  'pagingNavi': function (pTotalCnt) { //전체건수만큼 생성
+    var _ = this;
+    _.data['totalCnt'] = pTotalCnt ? pTotalCnt : _.data['totalCnt'];
+    if (_.data['totalCnt'] == 0) {
+      return "";
+    }
+    var pageNum2 = $("#pageIndex").val();
+    if (pageNum2 != "") {
+      _.data['currentPage'] = pageNum2;
+    }
+    _.data.totalPageCnt = Math.ceil(_.data.totalCnt / _.data.maxListCount);
+    var n_block = Math.ceil(_.data.currentPage / _.data.pageSize);
+
+    //페이징의 시작페이지와 끝페이지 구하기
+    var s_page = (n_block - 1) * _.data.pageSize + 1; // 현재블럭의 시작 페이지
+    var e_page = n_block * _.data.pageSize; // 현재블럭의 끝 페이지
+    var sb = '';
+    var sbTemp = '';
+
+    for (var j = s_page; j <= e_page; j++) {
+      if (j > _.data.totalPageCnt) break;
+      if (j == _.data.currentPage) {
+        sbTemp = sbTemp + "<a href='#' class='active'>" + j + "<span class='hide'>페이지로 이동</span></a>\n";
+      } else {
+        sbTemp = sbTemp + "<a href='javascript:PagingHelper.gotoPage(" + j + ");'>" + j + " <span class='hide'>페이지로 이동</span></a>\n";
+      }
+    }
+    sb = "<a href='javascript:PagingHelper.gotoPage(1);' class='page_arrow l2'><span class='hide'>첫페이지로 이동</span></a>\n";
+    if (_.data.currentPage < (_.data.pageSize + 1)) {
+      sb = sb + "<a href='#' class='page_arrow l1'><span class='hide'>이전 5페이지로 이동</span></a>\n";
+    } else {
+      sb = sb + "<a href='javascript:PagingHelper.gotoPage(" + (_.data.currentPage - 5) + ");' class='page_arrow l1'><span class='hide'>이전 5페이지로 이동</span></a>\n";
+    }
+    sb = sb + sbTemp
+
+    // 다음페이지 버튼
+    if (_.data.currentPage == _.data.totalPageCnt) {
+      pageNum = _.data.totalPageCnt;
+    } else if (_.data.currentPage < _.data.totalPageCnt) {
+      pageNum = Math.ceil(_.data.currentPage / _.data.pageSize) * _.data.pageSize + 1;
+    }
+    if (e_page >= _.data.totalPageCnt) {
+      pageNum = _.data.totalPageCnt;
+    }
+    sb = sb + "<a href='javascript:PagingHelper.gotoPage(" + (pageNum) + ");' class='page_arrow r1'><span class='hide'>다음 5페이지로 이동</span></a>\n";
+    sb = sb + "<a href='javascript:PagingHelper.gotoPage(" + (_.data.totalPageCnt) + ");' class='page_arrow r2'><span class='hide'>마지막 페이지로 이동</span></a>\n";
+    return sb;
+  },
+  'setStartnumEndnum': function () {
+    this.data.startnum = (this.data.currentPage - 1) * this.data.maxListCount + 1;
+
+    var tmp = this.data.currentPage * this.data.maxListCount;
+    this.data.lastnum = (tmp > this.data.totalCnt ? this.data.totalCnt : tmp);
+  },
+  'gotoPage': function (pageNum) {
+    if (pageNum < 0) {
+      pageNum = 1;
+    }
+    this.data.currentPage = pageNum;
+    this.setStartnumEndnum();
+    $("#paging").html(this.pagingNavi());
+    $("#pageIndex").val(pageNum);
+    fnSetData(pageNum);
+  }
+}
+
+
+const createSelectAreaList = () => {
+  let html = "";
+  $("#sidoAreaCd").empty();
+  html += '<option value="00">전국</option>';
+  $.each(upperAreaList, function (sidocd, sidonm) {
+    html += '<option value="' + sidocd + '">' + sidonm + '</option>';
+  });
+  $("#sidoAreaCd").append(html);
+}
+const createSelectSggList = (sidocd) => {
+  let html = ""; $("#sigunguAreaCd").empty();
+  if (sidocd == "00") {
+    html += '<option value="">지역을 선택해주세요.</option>';
+  } else {
+    html += '<option value="">전체</option>';
+    $.each(lowerAreaList[sidocd], function (sigungucd, sigungunm) {
+      html += '<option value="' + sigungucd + '">' + sigungunm + '</option>';
+    });
+  }
+  $("#sigunguAreaCd").append(html);
+}
+
+function enterkey() {
+  if (window.event.keyCode == 13) {
+    searchBtn();
+  }
+}
+
+
+
+//테이블폼생성
+function createtable(data) {
+  var dataList = new Array();
+  var row = '';
+  dataList = data;
+  var table = document.getElementById("table");
+  for (var i = 0; i < dataList.length; i++) {
+    row = row + "<tr>\n";
+    //	row = row + "<td class='num'>"+ dataList[i].seqnum + "</td>\n";
+
+    var cmpNm = document.getElementById("hidCmpNm").val();
+    var rprsvNm = document.getElementById("hidRprsvNm").val();
+    var bizRNo = document.getElementById("hidBizRNo").val();
+    var pageNo = document.getElementById("hidPg").val();
+    var areaCd = document.getElementById("hidArea").val();
+    var indstyCd = document.getElementById("hidIndsty").val();
+
+    var addParam = "";
+    if ((cmpNm != null && cmpNm != "")
+      || (rprsvNm != null && rprsvNm != "")
+      || (bizRNo != null && bizRNo != "")
+      || (pageNo != null && pageNo != "")) {
+      addParam = "&cmpNm=" + encodeURI(cmpNm) + "&rprsvNm=" + encodeURI(rprsvNm) + "&bizRNo=" + bizRNo + "&pageNo=" + pageNo + "&areaCd=" + areaCd + "&indstyCd=" + indstyCd;
+      /* row = row + "<td class='ta_l tit_td'><a href='/venturein/pbntc/searchVntrCmpDtls?vniaSn=" + dataList[i].vnia_sn + "&menuId=5000230" + addParam + "' class='c_tit'>" + dataList[i].cmp_nm + "</a></td>\n"; */
+      row = row + "<td class='ta_l tit_td'><a href='javascript:vntrCmpDtls(\"" + dataList[i].vnia_sn + "\", \"5000230\", \"" + addParam + "\");' class='c_tit'>" + dataList[i].cmp_nm + "</a></td>\n";
+
+    } else {
+      /* row = row + "<td class='ta_l tit_td'><a href='/venturein/pbntc/searchVntrCmpDtls?vniaSn=" + dataList[i].vnia_sn + "&menuId=5000230' class='c_tit'>" + dataList[i].cmp_nm + "</a></td>\n"; */
+      row = row + "<td class='ta_l tit_td'><a href='javascript:vntrCmpDtls(\"" + dataList[i].vnia_sn + "\");' class='c_tit'>" + dataList[i].cmp_nm + "</a></td>\n";
+    }
+
+    row = row + "<td>" + dataList[i].rprsv_nm + "</td>\n";
+    if (dataList[i].bizrno != null && dataList[i].bizrno != "") {
+      row = row + "<td class='ta_l'>" + dataList[i].bizrno.substr(0, 3) + "-" + dataList[i].bizrno.substr(3, 2) + "-" + dataList[i].bizrno.substr(5, 5) + "</td>\n";
+      /* row = row + "<td class='ta_l'>" + dataList[i].bizrno.substr(0, 3) + "-" + dataList[i].bizrno.substr(3, 2) + "-*****</td>\n"; */
+    } else {
+      row = row + "<td class='ta_l'></td>\n";
+
+    }
+    row = row + "<td class='ta_l'>" + dataList[i].hdofc_addr + "</td>\n";
+    row = row + "<td class='ta_l m_full'>" + dataList[i].indsty_nm + "</td>\n";
+    row = row + "</tr>\n";
+  }
+  if (dataList.length == 0) {
+    row = row + "<td colspan='6'>" + "조회된 값이 없습니다." + "</td>\n";
+  }
+  $("#table").empty();
+  $("#table").append(row);
+}
+
+function searchBtn() {
+  $("#pageIndex").val(1);
+  search();
+}
+
+function search() {
+  var pageNum = $("#pageIndex").val();
+  fnSetData(pageNum);
+}
+
+
+function fnSetData(pageNum) {
+  var sJson = {};
+  var cmpNm = $("#cmpNm").val();  //기업명
+  var rprsvNm = $("#rprsvNm").val(); //대표자명
+  var bizRNo = $("input[name=bizRNo]").val();  //사업자번호
+  var pageNo = pageNum;//페이지 번호
+  var pageSize = $("select[name=pageSize]").val();
+  var strArea = $("select[name=sidoAreaCd]").val();
+  var strSigunguArea = $("select[name=sigunguAreaCd]").val();
+  var strIndsty = $("select[name=selIndsty]").val();
+  if (strArea == "00") {
+    strArea = "";
+  }
+  sJson.cmpNm = cmpNm;
+  sJson.rprsvNm = rprsvNm;
+  sJson.bizRNo = bizRNo;
+  sJson.pg = pageNo;
+
+  document.getElementById("hidCmpNm").val() = cmpNm;
+  document.getElementById("hidRprsvNm").val() = rprsvNm;
+  document.getElementById("hidBizRNo").val() = bizRNo;
+  document.getElementById("hidPg").val() = pageNo;
+  document.getElementById("hidArea").val() = strArea;
+  document.getElementById("hidIndsty").val() = strIndsty;
+
+  $.ajax({
+    url: "/venturein/pbntc/searchVntrCmpAction",
+    type: "POST",
+    data: JSON.stringify({
+      "cmpNm": cmpNm
+      , "rprsvNm": rprsvNm
+      , "bizRNo": bizRNo
+      , "pg": pageNo
+      , "pageSize": pageSize
+      , "areaCd": strArea
+      , "sigunguAreaCd": strSigunguArea
+      , "indstyCd": strIndsty
+    }),
+    dataType: "json",
+    processData: true,
+    contentType: "application/json; charset=UTF-8",
+    success: function (data, textStatus, jqXHR) {
+      if (data.RESULT == "SUCCESS") {
+        createtable(data.DATA_LIST);
+      } else {
+        alert("조회 된 내역이 없습니다.");
+      }
+
+
+      var totRows = data.TOTAL_COUNT;
+
+      //전체 건수 totalcnt
+      $("#total").text("총  " + totRows + "건");
+      $("#paging").empty();
+      $("#paging").append(PagingHelper.pagingNavi(totRows));
+    },
+    error: function (data, textStatus) {
+      //alert(data.message);
+    }
+  });
+
+}
+
+  $(document).ready(function () {
+    searchBtn();
+    $("#selSigunguArea").append("<option id=selSigunguArea" + " value=" + "" + ">" + "지역을 선택해주세요" + "</option>");
+    //	fnSetData(1);
+
+    createSelectAreaList();
+    $("#sidoAreaCd").on('change', function () {
+      createSelectSggList($("#sidoAreaCd").val());
+    });
+  });
